b60650b4463e900be22a0423a8a367034744bd02
Liliana327 <894@holbertonschool.com>
2019-06-21 03:16:23 +0000

    0x01

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,27 @@
+/**
+ * main - Entry point
+ * Description: Program to write numbers 00 to 99 with a comma
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	int c;
+	int d;
+	for (c = 48; c <= 57; c++)
+	{
+		for (d = 48; d <= 57; d++)
+		{
+			putchar(c);
+			putchar(d);
+			if (c < 57 || d < 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
