8fd1dbc8ab9f42c74ce22f17ea0504b5ed3e139f
Liliana327 <894@holbertonschool.com>
2019-06-25 21:51:39 +0000

    0x02

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* _abs - function that computes the absolute value of an integer
+* @m: The character to print
+* Return: (0)
+*/
+int _abs(int m)
+{
+	if (m == 0)
+	{
+	return (m);
+	}
+	else if (m > 0)
+	{
+	return (m);
+	}
+	else
+	{
+	return (m * -1);
+	}
+	return (0);
+}
