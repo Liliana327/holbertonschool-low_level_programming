eaa01257f9e12bfb81f8a47654a54f8e12d72a77
Liliana327 <894@holbertonschool.com>
2019-06-27 22:31:36 +0000

    0x04

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_square - prints a square, followed by a new line
+ * description: prints a square, followed by a new line
+ * @size:  prints a square, followed by a new line
+ * return (0);
+ */
+void print_triangle(int size)
+{
+	int c;
+	int m;
+:
+	if (size <= 0)
+	{
+	_putchar ('\n');
+	}
+	else
+	{
+	for (c = 1 ; c <= size ; c++)
+	{
+	for (m = 1 ; m <= size ; m++)
+	{
+	_putchar ('#');
+	}
+	_putchar ('\n');
+	}
+}
+}
