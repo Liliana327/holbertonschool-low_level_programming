019d3ea9cd73dc83671b31a37805a855945a2dbb
Liliana327 <894@holbertonschool.com>
2019-07-02 20:01:20 +0000

    0x05

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,16 @@
 #include "holberton.h"
 /**
-* _puts - toma un valor de main
-* @a: puntero
-
-
+* _puts - toma la cadena de la funcion.
+* @str: value taken from main
 * Return: (0)
 */
-void _puts(char *str
+void _puts(char *str)
 {
-
+	int len;
+	
+	for (len = 0 ; str[len] != '\0' ; len++)
+	{
+	_putchar(str[len]);
+	}
+	_putchar('\n');
 }
