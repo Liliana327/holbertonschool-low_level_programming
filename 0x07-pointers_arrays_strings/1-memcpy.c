f395e16e5e200887cfbb48683003255d33c11309
Liliana327 <894@holbertonschool.com>
2019-07-09 01:20:10 +0000

    0x07

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,24 @@
 #include "holberton.h"
 /**
  * *_memcpy - Write a function that copies memory area.
- * @src: This is pointer to the destination array where the content is to be copied.
+ * @src: pointer to the destination array where the content is to be copied.
  * @dest: This is pointer to the source of data to be copied.
  * @n: This is the number of bytes to be copied.
  * Return: (dest)
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
- {
-	memcpy (dest,src,n);
-	return(des);
+{
+
+	char *m = src;
+	char *i = dest;
+	int a;
+
+	while (a < 0)
+	{
+	a = n;
+	if (a == 0)
+	i[a] = m[a];
+	a++;
+	}
+	return (dest);
 }
