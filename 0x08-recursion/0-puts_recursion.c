2587776285dfe940d584b35a933f9d4e4c73c484
Liliana327 <894@holbertonschool.com>
2019-07-10 17:09:05 +0000

    0x07

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* _puts_recursion - toma un valor de main
+* @s: puntero
+* Return: (0)
+*/
+void _puts_recursion(char *s)
+{
+	_putchar (*s);
+	if (*s != '\0')
+	{
+	_puts_recursion(s + 1);
+	s++;
+	}
+	else
+	{
+	_putchar ('\n');
+	}
+}
