b9c55ff5c0c2d8906b2b0f7bbba4b354d9b536cd
Liliana327 <894@holbertonschool.com>
2019-07-18 19:38:24 +0000

    0x0c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * longitud - length of a string
+ * @str: string t
+ * Return: 0;
+ */
+int longitud(char *str)
+{
+	int b, m;
+
+	m = 0;
+	for (b = 0; str && str[b] != '\0'; b++)
+	{
+		m++;
+	}
+	return (m);
+}
+
+/**
+ * str_concat - function that concatenates two strings
+ * @s1: 1 string
+ * @s2: 2 string
+ * n: variable
+ * Return: 0;
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *array;
+	int b, m;
+	unsigned int a;
+
+	b = 0;
+	m = longitud(s1) + n + 1;
+	array = malloc(sizeof(char) * m);
+	if (array == NULL)
+		return (NULL);
+	for (a = 0; s1 && s1[a] != '\0'; a++)
+	{
+		array[b] = s1[a];
+		b++;
+	}
+	for (a = 0; s2 && a < n; a++)
+	{
+		array[b] = s2[a];
+		b++;
+	}
+	array[b] = '\0'; 
+return (array);
+}
