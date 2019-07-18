64368084415b7fe1313a2f91d00b2c7704cfb2bc
Liliana327 <894@holbertonschool.com>
2019-07-18 17:07:47 +0000

    0x0c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * *malloc_checked - function that creates an array of chars
+ * @b: int the array
+ * Return: (*array)
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *array;
+	
+	array = malloc (b);
+	if (array == NULL)
+	{
+		exit(98);
+	}
+	return (array);
+}
