21f9b6d540ec859b78a731f6a27ed1ad1e91d1bd
Liliana327 <894@holbertonschool.com>
2019-07-24 20:46:50 +0000

    0x0F

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * int_index - imprime un nombre.
+ * @array: array
+ * @size: numero de elementos en la matrizarray
+ * @cmp: puntero que comparara el valor
+ * Return: Return.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int m;
+
+	if (array == NULL || cmp == NULL)
+	return (-1);
+	if (size <= 0)
+	return (-1);
+	for (m = 0; m < size; m++){
+	if (cmp(array[m]) != 0)
+	return (m);
+	}
+	return (-1);
+}
+
