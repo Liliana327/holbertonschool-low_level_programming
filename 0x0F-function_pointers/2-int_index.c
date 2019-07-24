7ab1f1cfdeb5b0a195b949dd28689542d132e9ed
Liliana327 <894@holbertonschool.com>
2019-07-24 20:48:37 +0000

    0x0F

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,7 +15,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	return (-1);
 	if (size <= 0)
 	return (-1);
-	for (m = 0; m < size; m++){
+	for (m = 0; m < size; m++)
+	{
 	if (cmp(array[m]) != 0)
 	return (m);
 	}
