1b5f7d4d41eb160acd2802927d81103a273e52c0
Liliana327 <894@holbertonschool.com>
2019-07-25 03:43:02 +0000

    000

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,18 +1,42 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "3-calc.h"
+/**
+*op_add - agrega 2 numeros
+*@a: numero1
+*@b: numero2
+*Return: resultado
+*/
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+/**
+*op_sub - resta 2 numeros
+*@a: numero1
+*@b: numero2
+*Return: resultado
+*/
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+/**
+*op_mul - multiplica 2 numeros
+*@a: numero1
+*@b: numero2
+*Return: resultado
+*/
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+*op_div - divide dos numeros
+*@a: numero1
+*@b: numero2
+*Return: resultado
+*/
 int op_div(int a, int b)
 {
 	return (a / b);
