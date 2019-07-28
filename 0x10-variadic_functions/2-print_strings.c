0ab6bb3b12caa2a6f238aeeda16a2124f88589eb
Liliana327 <894@holbertonschool.com>
2019-07-28 13:07:45 +0000

    cambio del punto 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,7 +13,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list arguments;
 	char *a;
 
-        va_start(arguments, n);
+	va_start(arguments, n);
 
 	for (m = 0; m < n; m++)
 	{
