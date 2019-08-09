c8a1057c6bb587d5c66b2f65979739ff6ea288cf
Liliana327 <894@holbertonschool.com>
2019-08-09 03:05:06 +0000

    add libreria

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,8 +1,7 @@
 #ifndef _HOLBERTON_H_
-#define _HOLBERTON_H
+#define _HOLBERTON_H_
 
 #include <stdio.h>
-#include <stdlib.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -11,4 +10,5 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-#endif /* _HOLBERTON_H_ */
+
+#endif
