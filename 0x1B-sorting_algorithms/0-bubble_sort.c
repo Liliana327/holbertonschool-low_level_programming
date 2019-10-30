432c104f2256202b614f4d4422ff83e477256a05
Liliana327 <894@holbertonschool.com>
2019-10-30 17:43:31 +0000

    add task number 0

--- 0x1B-sorting_algorithms/0-bubble_sort.c
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -1,29 +1,26 @@
 #include "sort.h"
 /*
+* bubble_sort - is responsible for arranging the arrangement
+* @array: is a pointer pointing to the list
+* @size: is the size of the list
+* Return: every change in the list
 */
-
-/*funcion que se encargara de ordenar el arreglo*/
 
 void bubble_sort(int *array, size_t size)
 {
-    unsigned int i, j;
-    int temp;
+	unsigned int i, j;
+	int temp;
 
 	for (i = 1; i < size; i++)
-	{		
+	{
 		for (j = 0; j < size - 1; j++)
-		
-			if (array[j] > array[j+1])
+
+			if (array[j] > array[j + 1])
 			{
 			temp = array[j];
-			array[j] = array[j+1];
-			array[j+1] = temp;
+			array[j] = array[j + 1];
+			array[j + 1] = temp;
 			print_array(array, size);
 			}
-        }
-        /*cuando la variable ordenado sea igual a 0
-          significara que el arreglo este ordenado
-          y no sera necesario seguir recorriendo el
-          arreglo para hacer comparaciones*/
-
+	}
 }
