76e976201c91ebe5050d826030c9ed5bb35122dc
Liliana327 <894@holbertonschool.com>
2019-10-30 22:09:13 +0000

    add task number 1

---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,67 @@
+#include "sort.h"
+/**
+* insertion_sort_list - Sort a doubly linked list.
+* @**list: ready to order
+*/
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *next = NULL;
+
+		if (list  == NULL)
+		if (*list == NULL)
+		if((*list)->next == NULL)
+        	return;
+
+	temp = *list;
+		while (temp->next)
+		{
+		next = temp->next;
+		sorted_insert(list, next);
+		temp = next;
+		}
+}
+
+/**
+* sorted_insert - function to insert a new node in sorted way in 
+* a sorted doubly linked list 
+* @**list: ready to order
+* @node:  the node to be compared
+*/
+void sorted_insert(listint_t **list, listint_t *node)
+{
+	listint_t *previous = NULL, *double_prev = NULL, *double_next = NULL;
+
+	previous = node->prev;
+
+	while (previous)
+	{
+		if (previous->n > node->n)
+			{
+			double_prev = previous->prev;
+			double_next = node->next;
+			node->next = previous;
+			previous->prev = node;
+				if (!double_prev)
+				{	
+				*list = node;
+				(*list)->prev = NULL;
+				}
+				else
+				{
+				node->prev = double_prev;
+				double_prev->next = node;
+				}			
+				if (double_next)
+				{
+				previous->next = double_next;
+				double_next->prev = previous;
+				}
+				else
+				previous->next = NULL;
+				print_list((const listint_t *)*list);
+        		}
+			else
+			return;
+			previous = node->prev;
+	}
+}
