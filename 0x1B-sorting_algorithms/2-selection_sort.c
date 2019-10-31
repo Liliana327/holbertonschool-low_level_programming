c0c6b519b0ec3affd33fe8bad34e15e2eff07434
Liliana327 <894@holbertonschool.com>
2019-10-31 03:00:38 +0000

    add task number 2

---
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+* bubble_sort - is responsible for arranging the arrangement
+* @array: is a pointer pointing to the list
+* @size: is the size of the list
+* Return: every change in the list
+*/
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j;
+	int temp;
+
+	for (i = 1; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+
+			if (array[j] > array[j + 1])
+			{
+			temp = array[j];
+			array[j] = array[j + 1];
+			array[j + 1] = temp;
+			print_array(array, size);
+			}
+	}
+}
