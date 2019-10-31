b7133a8c23fe1a239ad35a47acda49f100d27b20
Liliana327 <894@holbertonschool.com>
2019-10-31 03:43:47 +0000

    add

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -1,10 +1,11 @@
 #include "sort.h"
 /**
-* bubble_sort - is responsible for arranging the arrangement
+* selection_sort - is responsible for arranging the arrangement
 * @array: is a pointer pointing to the list
 * @size: is the size of the list
 * Return: every change in the list
 */
+
 void selection_sort(int *array, size_t size)
 {
 	unsigned int i, j;
