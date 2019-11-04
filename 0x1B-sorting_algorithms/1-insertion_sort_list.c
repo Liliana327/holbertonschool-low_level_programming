104d697d00d3300e24882ae157473e1c654823e0
Liliana327 <894@holbertonschool.com>
2019-11-04 04:14:31 +0000

    add task number 1

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -5,63 +5,57 @@
 */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp, *next = NULL;
+	listint_t *temp, *nexts, current;
 
-		if (list  == NULL)
-		if (*list == NULL)
-		if ((*list)->next == NULL)
-		return;
+	if (!list || !*lis)
+	return;
 
-	temp = *list;
-		while (temp->next)
+	current = *list;
+	while (current && current->next)
+	{
+		if (current->n > current->n)
 		{
-		next = temp->next;
-		sorted_insert(list, next);
-		temp = next;
+			sorted_insert(current, current->next, list);
+			nexts = current;
+			current = current->prev;
+			while (current && current->prev)
+			{
+				tmp = current;
+				if (tmp->n < tmp->prev->n)
+					sorted_insert(tmp->prev, tmp, list);
+				else
+				break;
+			}
+			current = nexts;
 		}
+		else
+		current = current->next;
+	}
 }
 
 /**
 * sorted_insert - function to insert a new node in sorted way in
 * a sorted doubly linked list
 * @list: ready to order
-* @node:  the node to be compared
+* @left: left the node to be compared
+* @right: right the node to be compared
 */
-void sorted_insert(listint_t **list, listint_t *node)
+void sorted_insert(listint_t *left, listint_t *right, listint_t **list)
 {
-	listint_t *previous = NULL, *double_prev = NULL, *double_next = NULL;
+	listint_t *node, *previous;
 
-	previous = node->prev;
+	node = right->next;
+	previous = left->prev;
 
-	while (previous)
-	{
-		if (previous->n > node->n)
-			{
-			double_prev = previous->prev;
-			double_next = node->next;
-			node->next = previous;
-			previous->prev = node;
-				if (!double_prev)
-				{
-				*list = node;
-				(*list)->prev = NULL;
-				}
-				else
-				{
-				node->prev = double_prev;
-				double_prev->next = node;
-				}
-				if (double_next)
-				{
-				previous->next = double_next;
-				double_next->prev = previous;
-				}
-				else
-				previous->next = NULL;
-				print_list((const listint_t *)*list);
-	}
-			else
-			return;
-			previous = node->prev;
-	}
+	if (previous)
+		previous->next = right;
+	else
+		*list = right;
+	if (node)
+		node->prev = left;
+	right->prev = previous;
+	left->next = node;
+	right->next = left;
+	left->prev = right;
+	print_list(*list);
 }
