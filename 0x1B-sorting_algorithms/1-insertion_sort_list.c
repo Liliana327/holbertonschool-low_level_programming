b4ffd364044b764957a01859eb622c1b037e93f3
Liliana327 <894@holbertonschool.com>
2019-11-04 05:13:36 +0000

    modified task number 1

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -1,61 +1,62 @@
 #include "sort.h"
 /**
-* insertion_sort_list - Sort a doubly linked list.
-* @list: ready to order
-*/
-void insertion_sort_list(listint_t **list)
-{
-	listint_t *temp, *nexts, current;
-
-	if (!list || !*lis)
-	return;
-
-	current = *list;
-	while (current && current->next)
-	{
-		if (current->n > current->n)
-		{
-			sorted_insert(current, current->next, list);
-			nexts = current;
-			current = current->prev;
-			while (current && current->prev)
-			{
-				tmp = current;
-				if (tmp->n < tmp->prev->n)
-					sorted_insert(tmp->prev, tmp, list);
-				else
-				break;
-			}
-			current = nexts;
-		}
-		else
-		current = current->next;
-	}
-}
-
-/**
 * sorted_insert - function to insert a new node in sorted way in
 * a sorted doubly linked list
 * @list: ready to order
 * @left: left the node to be compared
 * @right: right the node to be compared
 */
+
 void sorted_insert(listint_t *left, listint_t *right, listint_t **list)
 {
 	listint_t *node, *previous;
 
-	node = right->next;
-	previous = left->prev;
-
-	if (previous)
-		previous->next = right;
+	node = left->prev;
+	previous = right->next;
+	if (node)
+		node->next = right;
 	else
 		*list = right;
-	if (node)
-		node->prev = left;
-	right->prev = previous;
-	left->next = node;
-	right->next = left;
-	left->prev = right;
-	print_list(*list);
+	if (previous)
+		previous->prev = left;
+		right->prev = node;
+		left->next = previous;
+		right->next = left;
+		left->prev = right;
+		print_list(*list);
+}
+
+/**
+* insertion_sort_list - Sort a doubly linked list.
+* @list: ready to order
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temp, *nexts, *current;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	current = *list;
+	while (current && current->next)
+	{
+		if (current->n > current->next->n)
+		{
+			sorted_insert(current, current->next, list);
+			nexts= current;
+			current = current->prev;
+			while (current && current->prev)
+			{
+				temp = current;
+				if (temp->n < temp->prev->n)
+					sorted_insert(temp->prev, temp, list);
+				else
+				break;
+			}
+			current = nexts;
+		}
+		else
+			current = current->next;
+	}
 }
--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -22,6 +22,7 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void sorted_insert(listint_t **list, listint_t *node);
+void insertion_sort_list(listint_t **list);
+void sorted_insert(listint_t *left, listint_t *right, listint_t **list);
 void selection_sort(int *array, size_t size);
 #endif /* _SORT_H */
