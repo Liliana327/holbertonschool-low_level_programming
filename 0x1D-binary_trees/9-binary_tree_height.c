ddcfb59b4517ce3fd3c3ea6f8c1c0dc6430f9f84
Liliana327 <894@holbertonschool.com>
2019-12-07 03:10:49 +0000

    modifaile

--- 0x1D-binary_trees/9-binary_tree_height.c
+++ 0x1D-binary_trees/9-binary_tree_height.c
@@ -15,7 +15,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	tall = binary_tree_height(tree->left);
 	low = binary_tree_height(tree->right);
-	if (tall < low)
+	if (tall > low)
 		return (tall + 1);
-	return (tall + 1);
+	else
+		return (low + 1);
 }
