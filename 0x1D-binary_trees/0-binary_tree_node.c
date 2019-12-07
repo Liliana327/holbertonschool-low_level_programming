b4b7e614e58efa8639500d948d0de582f8ea3f6b
Liliana327 <894@holbertonschool.com>
2019-12-07 00:56:35 +0000

    modifaile documentation

--- 0x1D-binary_trees/0-binary_tree_node.c
+++ 0x1D-binary_trees/0-binary_tree_node.c
@@ -3,7 +3,7 @@
 * binary_tree_node - Function that creates a binary tree node
 * @parent: Edge up to a node called father.
 * @value: The value of the new nodes.
-* return: Value the new node.
+* Return: Value the new node.
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
